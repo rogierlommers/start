e11adf12f86800e34cfb2169b83db929d71a6436
Rogier Lommers <rogier@lommers.org>
2026-04-22 16:54:45 +0200

    feat: embed HTML content and update appHome handler to serve index.html

--- internal/httpweb/routes.go
+++ internal/httpweb/routes.go
@@ -1,11 +1,15 @@
 package httpweb
 
 import (
+	_ "embed"
 	"net/http"
 	"start/internal/service"
 
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed web/index.html
+var indexHTML []byte
 
 type handlers struct {
 	svc *service.Service
@@ -19,7 +23,5 @@ func Register(router gin.IRouter, svc *service.Service) {
 }
 
 func (h handlers) appHome(c *gin.Context) {
-
-	html := "<!doctype html><html><head><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><title>start</title></head><body><main><h1>start dashboard</h1></main></body></html>"
-	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(html))
+	c.Data(http.StatusOK, "text/html; charset=utf-8", indexHTML)
 }
